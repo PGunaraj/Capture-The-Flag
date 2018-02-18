6466fbc89626e12aa64b35ef7c7574facc473ddc
Priyadharshini Gunaraj <pgunaraj@asu.edu>
2018-02-17 18:36:14 -0800

    Update ictflogin.py

--- Setup_Script/ictflogin.py
+++ Setup_Script/ictflogin.py
@@ -4,7 +4,7 @@ import os
 #Update IP
 ip = "35.160.215.67"
 #Update Team name
-team = "kselladu@asu.edu"
+team = "pgunaraj@asu.edu"
 i = iCTF("http://%s/" % ip)
 #Update Password
 t = i.login(team,"3VXEHUbdM4FG")
